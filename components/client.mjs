f6e4b1f298d37e8b0135be6e9637fa20cf032f3a
KaMeHb-UA <marlock@etlgr.com>
2019-06-14 17:34:14 +0300

    initial client lib part

--- components/client.mjs
+++ components/client.mjs
@@ -1,5 +1,34 @@
 import socketIO from './socketIO.mjs'
 
-export default async controller => {
-    const socket = (await socketIO)();
+export default controller => {
+    let socket = socketIO.then(s => s());
+
+    function method(name, ...args){
+        return new Promise(r => {
+            socket.emit('method', name, ...args, r)
+        })
+    }
+
+    (async () => {
+        const sio = await socket;
+        const ctrlr = new (await controller)(sio);
+        sio.on('broadcast', (name, ...args) => {
+            if(ctrlr[name] && typeof ctrlr[name] === 'function'){
+                ctrlr[name](...args)
+            }
+        });
+        sio.on('client_update', (name, ...args) => {
+            if(ctrlr[name] && typeof ctrlr[name] === 'function'){
+                ctrlr[name](...args)
+            }
+        });
+    })();
+
+    return new Proxy({}, {
+        get(t, p){
+            if(!t[p]) t[p] = method.bind(null, p);
+            return t[p]
+        },
+        set(t, p, v){ return false },
+    })
 }
