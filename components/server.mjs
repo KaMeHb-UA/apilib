dbfd1997cb6cbb5ef54bbf08919cd0b891b0f9e4
KaMeHb-UA <marlock@etlgr.com>
2019-06-14 18:04:48 +0300

    tmp

--- components/server.mjs
+++ components/server.mjs
@@ -2,4 +2,10 @@ import socketIO from './socketIO.mjs'
 
 export default async controller => {
     const io = (await socketIO)();
+    io.on('connect', async socket => {
+        const ctrlr = new (await controller)(socket);
+        socket.on('method', (name, ...args) => {
+            const r = args.pop();
+        })
+    })
 }
