64f94b3de96d363324e38a6da9c534a5a95c6b5b
KaMeHb-UA <marlock@etlgr.com>
2019-07-08 02:02:49 +0300

    fixed support for node >=10.12.0

--- components/socketIO-server.mjs
+++ components/socketIO-server.mjs
@@ -1,4 +1,8 @@
 import Module from 'module';
-const require = Module.createRequire(import.meta.url);
+let require;
+if(Module.createRequire) require = Module.createRequire(import.meta.url); else {
+    if(!Module.createRequireFromPath) throw new Error('There is no way to create legacy require function. Please check you are using supported node version. Minimum supported version: 10.12.0. Your version: ' + process.version + '. Aborting...');
+    require = Module.createRequireFromPath(decodeURI(import.meta.url).slice(7))
+}
 const socketIO = require('socket.io');
 export default socketIO
